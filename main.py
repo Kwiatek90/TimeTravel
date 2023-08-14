3401a79a8f686e7f1ed1d01221a22d6400bc486c
Kwiatek90 <mateusz.kwiat9@gmail.com>
2023-08-14 22:46:53 +0200

    Desc first room

--- main.py
+++ main.py
@@ -45,12 +45,33 @@ class Table(Furniture):
         self.solution = solution
         
     def opis(self):
-        print(f"\nPodchodząc do stołu zauważasz tekst.\n"
-              + "TUTAJ ZAGADKA\n"
+        print(f"\nNa stole leży stary, zżółkły pergamin.\n"
+              + "'Przez ocean żeglarz dzielny,\n"
+              + "Nowe lądy odkrył ciekawy,\n"
+              + "Amerykę znalazł pewnego dnia,\n"
+              + "Nazwisko jego brzmi jak...?'\n"
               + "Pod tekstem znajduję się klawiatura, a nad nią mały ekran\n" 
-              + "ktory wyświetla wpisany litery przez klawiaturę\n"
+              + "ktory wyświetla wpisane litery przez klawiaturę\n"
         )
               
+      
+class Machine(Furniture):
+    #tablica do wrzucania monet
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def opis(self):
+        print(f"Maszyna jest największym elementem pomieszczenia. Jej konstrukcja przypomina\n"
+            + "kombinację zegara, urządzenia muzycznego i maszyny do gry. Skomplikowane rury i\n"
+            + "przewody prowadzą do różnych sekcji, a każdy element jest zdobiony symbolami\n"
+            + "związanymi z podróżami w czasie.\n")
+     
+    def use(self):
+        print("Przechodzisz zza maszynę, widzisz na niej instrukcję obsługi:\n"
+              + "Musisz odnależć medaliony, by dostosować maszynę w odpowiedni sposób i\n"
+              + "aktywować magiczne pole energii, które umożliwi Ci podróż w czasie!\n"
+              + "Aby to osiągnąć, musisz zidentyfikować właściwe wzorce i sekwencje, które reprezentują różne okresy czasu.\n")
+         
         
    
 
@@ -88,14 +109,18 @@ def play_first_room():
                 print("Podaj rozwiązanie tej zagadki:\n")
                 solution = input()
                 if solution == table.solution:
-                    print("Drzwi do pokoju drugie sie otworzyły!")
+                    print("Po wpisaniu hasła, nagle właz się poruszył, być może się otworzyl?")
                     Player.add_level()
                     choosing_menu()
                 else:
                     print("Błedna odpowiedz")
                 pass
         elif choose == r"\maszyna":
-            pass
+            maszyna = Machine("Maszyna")
+            maszyna.opis()
+            option = choosing_menu()
+            if option == r"\get":
+                maszyna.use()
         elif choose == r"\wlaz":
             if Player.level == 0:
                 print("Właz jest zamknięty")
@@ -108,7 +133,26 @@ def play_first_room():
         
 def play_second_room():
     while True:
-        print("Drugi pokój")
+        print(r"Wybierz gdzie chcesz podejśc: \telefon, \maszyna_czasu, \kufer, \ksiega, \waga, \szkatulka, \wlaz")
+        choose = input()
+        if choose == r"\telefon":
+            pass
+        elif choose == r"\maszyna_czasu":
+            pass
+        elif choose == r"\kufer":
+            pass
+        elif choose == r"\ksiega":
+            pass
+        elif choose == r"\waga":
+            pass
+        elif choose == r"\szkatulka":
+            pass
+        elif choose == r"\wlaz":
+           play_first_room()
+        else:
+            print("Źle")
+            continue
+        
         
     
 def choosing_menu():
@@ -133,7 +177,17 @@ def choosing_menu():
             continue
      
     
-print("Opis gry\n")
-print("Trafiłeś do pokoju w którym znajduje się: \n-tablica na scianie\n-stół metalowy\n-maszyna z otworem u góry\n-właz zamknietym wielki zasuwami\n")
+print("W grze wcielasz się w rolę utalentowanego archeologa, który przypadkowo odkrywa\n"
+      + "tajemnicze pomieszczenie w jednym z budynków w Barcelonie. Okazuje się, że to jest\n"
+      + "wejście do niewyobrażalnego świata podróży w czasie. Twoim zadaniem jest odkrycie\n"
+      + "sekretów tego miejsca i ocalenie historii.\n")
+
+
+print("Tajemniczy pokój\n"
+      + "Przy wejściu do pomieszczenia archeolog znajduje się w pokoju o stonowanej kolorystyce.\n"
+      + "Ściany zdobią starodawne mapy i tablice związane z podróżami w czasie. Na środku stoi stół\n"
+      + "z metalowym blatem, na którym leży zakurzony stary pergamin.\n"
+      + "W rogu stoju tajemnicza maszyna. Obok niej znajduję się stary, trudny do zauważenia właz\n")
+
 player = Player()
 play_first_room()
